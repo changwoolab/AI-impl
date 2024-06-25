de718a868e77da5cf3e1ad7a4140f139eda6551b
changwoolab <cwyoo01@naver.com>
2024-06-25 23:56:15 +0900

    Dataloader, Transform

--- unet_segmentation/train.py
+++ unet_segmentation/train.py
@@ -6,6 +6,8 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
+import matplotlib.pyplot as plt
+
 from torchvision import transforms, datasets
 
 # 트레이닝 파라미터 설정
@@ -129,3 +131,126 @@ class UNet(nn.Module):
         x = self.fc(dec1_1)
 
         return x
+    
+## 데이터 로더
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir
+        self.transform = transform
+    
+        lst_data = os.listdir(self.data_dir)
+
+        lst_label = [f for f in lst_data if f.startswith('label')]
+        lst_input = [f for f in lst_data if f.startswith('input')]
+
+        lst_label.sort()
+        lst_input.sort()
+
+        self.lst_label = lst_label
+        self.lst_input = lst_input
+
+    def __len__(self):
+        return len(self.lst_label)
+    
+    def __getitem__(self, index):
+        label = np.load(os.path.join(self.data_dir, self.lst_label[index]))
+        input = np.load(os.path.join(self.data_dir, self.lst_input[index]))
+
+        # 0~1 사이로 Normalize (range가 0~255)
+        label = label / 255.0
+        input = input / 255.0
+
+        # channel axis를 3개로 맞춰주기
+        if label.ndim == 2:
+            label = label[:, :, np.newaxis]
+        if input.ndim == 2:
+            input = input[:, :, np.newaxis]
+
+        data = {'input': input, 'label': label}
+
+        if self.transform:
+            data = self.transform(data)
+        
+        return data
+
+## Dataset test
+# dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'))
+
+# data = dataset_train.__getitem__(0)
+
+# input = data['input']
+# label = data['label']
+
+# plt.subplot(121)
+# plt.imshow(input.squeeze())
+
+# plt.subplot(122)
+# plt.imshow(label.squeeze())
+
+# plt.show()
+
+## Transform 구현
+
+# Numpy -> Tensor
+class ToTensor(object):
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+
+        # Image의 numpy 차원 = (Y, X, Channel)
+        # PyTorch의 tensor 차원 = (Channel, Y, X)
+        label = label.transpose((2, 0, 1)).astype(np.float32)
+        input = input.transpose((2, 0, 1)).astype(np.float32)
+
+        data = {'label': torch.from_numpy(label), 'input': torch.from_numpy(input)}
+
+        return data
+
+
+class Normalization(object):
+    def __init__(self, mean=0.5, std=0.5):
+        self.mean = mean
+        self.std = std
+    
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+
+        input = (input - self.mean) / self.std
+
+        data = {'label': label, 'input': input}
+
+        return data
+
+# 좌우 상하로 랜덤하게 뒤집기
+class RandomFlip(object):
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+
+        if np.random.rand() > 0.5:
+            label = np.fliplr(label)
+            input = np.fliplr(input)
+
+        if np.random.rand() > 0.5:
+            label = np.flipud(label)
+            input = np.flipud(input)
+        
+        data = {'label': label, 'input': input}
+
+        return data
+
+## Transform test
+# transform = transforms.Compose([Normalization(mean=0.5, std=0.5), RandomFlip(), ToTensor()])
+
+# dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'), transform=transform)
+
+# data = dataset_train.__getitem__(0)
+
+# input = data['input']
+# label = data['label']
+
+# plt.subplot(121)
+# plt.imshow(input.squeeze())
+
+# plt.subplot(122)
+# plt.imshow(label.squeeze())
+
+# plt.show()
