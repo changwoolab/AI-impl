1e5ae88c21cac60ff7f8903398e779633cf05d0d
changwoolab <cwyoo01@naver.com>
2024-06-26 23:32:44 +0900

    refactor train

--- unet_segmentation/train.py
+++ unet_segmentation/train.py
@@ -1,3 +1,5 @@
+import argparse
+
 import os
 import numpy as np
 
@@ -10,216 +12,33 @@ import matplotlib.pyplot as plt
 
 from torchvision import transforms, datasets
 
-# 트레이닝 파라미터 설정
-lr = 1e-3
-batch_size = 4
-num_epoch = 100
+from model import UNet
+from dataset import *
+from util import *
 
-data_dir = './datasets'
-ckpt_dir = './checkpoint'
-log_dir = './log'
+## Arg Parser
+parser = argparse.ArgumentParser(description="Train the UNet",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--lr", default=1e-3, type=float, dest="lr")
+parser.add_argument('--batch_size', default=4, type=int, dest='batch_size')
+parser.add_argument('--num_epoch', default=100, type=int, dest='num_epoch')
+
+parser.add_argument('--data_dir', default='./datasets', type=str, dest='data_dir')
+parser.add_argument('--ckpt_dir', default='./checkpoint', type=str, dest='ckpt_dir')
+parser.add_argument('--log_dir', default='./log', type=str, dest='log_dir')
+
+args = parser.parse_args()
+
+# 트레이닝 파라미터 설정
+lr = args.lr
+batch_size = args.batch_size
+num_epoch = args.num_epoch
+
+data_dir = args.data_dir
+ckpt_dir = args.ckpt_dir
+log_dir = args.log_dir
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# 네트워크 구축하기
-class UNet(nn.Module):
-    def __init__(self):
-        super(UNet, self).__init__()
-
-        # Convolution, BatchNormalization, ReLU
-        def CBR2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True):
-            layers = []
-            layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                                kernel_size=kernel_size, stride=stride,
-                                padding=padding, bias=bias)]
-            layers += [nn.BatchNorm2d(num_features=out_channels)]
-            layers += [nn.ReLU()]
-
-            cbr = nn.Sequential(*layers)
-
-            return cbr
-    
-        # Contracting path
-        self.enc1_1 = CBR2d(in_channels=1, out_channels=64)
-        self.enc1_2 = CBR2d(in_channels=64, out_channels=64)
-
-        self.pool1 = nn.MaxPool2d(kernel_size=2)
-
-        self.enc2_1 = CBR2d(in_channels=64, out_channels=128)
-        self.enc2_2 = CBR2d(in_channels=128, out_channels=128)
-
-        self.pool2 = nn.MaxPool2d(kernel_size=2)
-
-        self.enc3_1 = CBR2d(in_channels=128, out_channels=256)
-        self.enc3_2 = CBR2d(in_channels=256, out_channels=256)
-
-        self.pool3 = nn.MaxPool2d(kernel_size=2)
-
-        self.enc4_1 = CBR2d(in_channels=256, out_channels=512)
-        self.enc4_2 = CBR2d(in_channels=512, out_channels=512)
-
-        self.pool4 = nn.MaxPool2d(kernel_size=2)
-
-        self.enc5_1 = CBR2d(in_channels=512, out_channels=1024)
-
-        # Expansive path
-        self.dec5_1 = CBR2d(in_channels=1024, out_channels=512)
-
-        self.unpool4 = nn.ConvTranspose2d(in_channels=512, out_channels=512, kernel_size=2, stride=2, padding=0, bias=True)
-
-        self.dec4_2 = CBR2d(in_channels=2*512, out_channels=512)
-        self.dec4_1 = CBR2d(in_channels=512, out_channels=256)
-
-        self.unpool3 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=2, stride=2, padding=0, bias=True)
-
-        self.dec3_2 = CBR2d(in_channels=2*256, out_channels=256)
-        self.dec3_1 = CBR2d(in_channels=256, out_channels=128)
-
-        self.unpool2 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=2, stride=2, padding=0, bias=True)
-
-        self.dec2_2 = CBR2d(in_channels=2*128, out_channels=128)
-        self.dec2_1 = CBR2d(in_channels=128, out_channels=64)
-
-        self.unpool1 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=2, stride=2, padding=0, bias=True)
-
-        self.dec1_2 = CBR2d(in_channels=2*64, out_channels=64)
-        self.dec1_1 = CBR2d(in_channels=64, out_channels=64)
-
-        self.fc = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0, bias=True)
-
-    def forward(self, x):
-        enc1_1 = self.enc1_1(x)
-        enc1_2 = self.enc1_2(enc1_1)
-        pool1 = self.pool1(enc1_2)
-
-        enc2_1 = self.enc2_1(pool1)
-        enc2_2 = self.enc2_2(enc2_1)
-        pool2 = self.pool2(enc2_2)
-
-        enc3_1 = self.enc3_1(pool2)
-        enc3_2 = self.enc3_2(enc3_1)
-        pool3 = self.pool3(enc3_2)
-
-        enc4_1 = self.enc4_1(pool3)
-        enc4_2 = self.enc4_2(enc4_1)
-        pool4 = self.pool4(enc4_2)
-
-        enc5_1 = self.enc5_1(pool4)
-
-        dec5_1 = self.dec5_1(enc5_1)
-
-        unpool4 = self.unpool4(dec5_1)
-        cat4 = torch.cat((unpool4, enc4_2), dim=1)
-        dec4_2 = self.dec4_2(cat4)
-        dec4_1 = self.dec4_1(dec4_2)
-
-        unpool3 = self.unpool3(dec4_1)
-        cat3 = torch.cat((unpool3, enc3_2), dim=1)
-        dec3_2 = self.dec3_2(cat3)
-        dec3_1 = self.dec3_1(dec3_2)
-
-        unpool2 = self.unpool2(dec3_1)
-        cat2 = torch.cat((unpool2, enc2_2), dim=1)
-        dec2_2 = self.dec2_2(cat2)
-        dec2_1 = self.dec2_1(dec2_2)
-
-        unpool1 = self.unpool1(dec2_1)
-        cat1 = torch.cat((unpool1, enc1_2), dim=1)
-        dec1_2 = self.dec1_2(cat1)
-        dec1_1 = self.dec1_1(dec1_2)
-
-        x = self.fc(dec1_1)
-
-        return x
-    
-## 데이터 로더
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self, data_dir, transform=None):
-        self.data_dir = data_dir
-        self.transform = transform
-    
-        lst_data = os.listdir(self.data_dir)
-
-        lst_label = [f for f in lst_data if f.startswith('label')]
-        lst_input = [f for f in lst_data if f.startswith('input')]
-
-        lst_label.sort()
-        lst_input.sort()
-
-        self.lst_label = lst_label
-        self.lst_input = lst_input
-
-    def __len__(self):
-        return len(self.lst_label)
-    
-    def __getitem__(self, index):
-        label = np.load(os.path.join(self.data_dir, self.lst_label[index]))
-        input = np.load(os.path.join(self.data_dir, self.lst_input[index]))
-
-        # 0~1 사이로 Normalize (range가 0~255)
-        label = label / 255.0
-        input = input / 255.0
-
-        # channel axis를 3개로 맞춰주기
-        if label.ndim == 2:
-            label = label[:, :, np.newaxis]
-        if input.ndim == 2:
-            input = input[:, :, np.newaxis]
-
-        data = {'input': input, 'label': label}
-
-        if self.transform:
-            data = self.transform(data)
-        
-        return data
-
-## Transform 구현
-
-# Numpy -> Tensor
-class ToTensor(object):
-    def __call__(self, data):
-        label, input = data['label'], data['input']
-
-        # Image의 numpy 차원 = (Y, X, Channel)
-        # PyTorch의 tensor 차원 = (Channel, Y, X)
-        label = label.transpose((2, 0, 1)).astype(np.float32)
-        input = input.transpose((2, 0, 1)).astype(np.float32)
-
-        data = {'label': torch.from_numpy(label), 'input': torch.from_numpy(input)}
-
-        return data
-
-
-class Normalization(object):
-    def __init__(self, mean=0.5, std=0.5):
-        self.mean = mean
-        self.std = std
-    
-    def __call__(self, data):
-        label, input = data['label'], data['input']
-
-        input = (input - self.mean) / self.std
-
-        data = {'label': label, 'input': input}
-
-        return data
-
-# 좌우 상하로 랜덤하게 뒤집기
-class RandomFlip(object):
-    def __call__(self, data):
-        label, input = data['label'], data['input']
-
-        if np.random.rand() > 0.5:
-            label = np.fliplr(label)
-            input = np.fliplr(input)
-
-        if np.random.rand() > 0.5:
-            label = np.flipud(label)
-            input = np.flipud(input)
-        
-        data = {'label': label, 'input': input}
-
-        return data
 
 ## 네트워크 학습
 transform = transforms.Compose([Normalization(mean=0.5, std=0.5), RandomFlip(), ToTensor()])
@@ -254,31 +73,6 @@ fn_class = lambda x: 1.0 * (x > 0.5)
 ## Tensorboard를 사용하기 위한 SummaryWriter 설정
 writer_train = SummaryWriter(log_dir=os.path.join(log_dir, 'train'))
 writer_val = SummaryWriter(log_dir=os.path.join(log_dir, 'val'))
-
-## 네트워크 저장하기
-def save(ckpt_dir, net, optim, epoch):
-    if not os.path.exists(ckpt_dir):
-        os.makedirs(ckpt_dir)
-    
-    torch.save({'net': net.state_dict(), 'optim': optim.state_dict()},
-                "./%s/model_epoch%d.pth" % (ckpt_dir, epoch))
-    
-## 네트워크 불러오기
-def load(ckpt_dir, net, optim):
-    if not os.path.exists(ckpt_dir):
-        epoch = 0
-        return net, optim, epoch
-    
-    ckpt_lst = os.listdir(ckpt_dir)
-    ckpt_lst.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
-
-    dict_model = torch.load('./%s/%s' % (ckpt_dir, ckpt_lst[-1]))
-
-    net.load_state_dict(dict_model['net'])
-    optim.load_state_dict(dict_model['optim'])
-    epoch = int(ckpt_lst[-1].split('epoch')[1].split('.pth')[0])
-
-    return net, optim, epoch
 
 ## 네트워크 학습시키기
 st_epoch = 0
