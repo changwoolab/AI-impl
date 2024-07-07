4dacc1d08166e6d65b20758c8fd76c8f54d3d123
changwoolab <cwyoo01@naver.com>
2024-07-07 22:58:47 +0900

    Dataset 구현

--- unet_segmentation-regression/dataset.py
+++ unet_segmentation-regression/dataset.py
@@ -4,39 +4,49 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+import matplotlib.pyplot as plt
+
+from util import *
+
 ## 데이터 로더
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, data_dir, transform=None):
+    def __init__(self, data_dir, transform=None, task=None, opts=None):
         self.data_dir = data_dir
         self.transform = transform
+        self.task = task
+        self.opts = opts
     
         lst_data = os.listdir(self.data_dir)
+        lst_data = [f for f in lst_data if f.endswith('jpg') | f.endswith('png')]
 
-        lst_label = [f for f in lst_data if f.startswith('label')]
-        lst_input = [f for f in lst_data if f.startswith('input')]
-
-        lst_label.sort()
-        lst_input.sort()
-
-        self.lst_label = lst_label
-        self.lst_input = lst_input
+        lst_data.sort()
+        
+        self.lst_data = lst_data
 
     def __len__(self):
-        return len(self.lst_label)
+        return len(self.lst_data)
     
     def __getitem__(self, index):
-        label = np.load(os.path.join(self.data_dir, self.lst_label[index]))
-        input = np.load(os.path.join(self.data_dir, self.lst_input[index]))
+        img = plt.imread(os.path.join(self.data_dir, self.lst_data[index]))
+        sz = img.shape
 
-        # 0~1 사이로 Normalize (range가 0~255)
-        label = label / 255.0
-        input = input / 255.0
+        if sz[0] > sz[1]:
+            img = img.transpose((1, 0, 2))
 
-        # channel axis를 3개로 맞춰주기
-        if label.ndim == 2:
-            label = label[:, :, np.newaxis]
-        if input.ndim == 2:
-            input = input[:, :, np.newaxis]
+        if img.dtype == np.uint8:
+            img = img / 255.0
+
+        if img.ndim == 2:
+            img = img[:, :, np.newaxis]
+        
+        label = img
+
+        if self.task == 'denoising':
+            input = add_noise(img, type=self.opts[0], opts=self.opts[1])
+        elif self.task == 'inpainting':
+            input = add_sampling(img, type=self.opts[0], opts=self.opts[1])
+        elif self.task == 'super_resolution':
+            input = add_blur(img, type=self.opts[0], opts=self.opts[1])
 
         data = {'input': input, 'label': label}
 
@@ -70,6 +80,7 @@ class Normalization(object):
     def __call__(self, data):
         label, input = data['label'], data['input']
 
+        label = (label - self.mean) / self.std
         input = (input - self.mean) / self.std
 
         data = {'label': label, 'input': input}
@@ -90,5 +101,28 @@ class RandomFlip(object):
             input = np.flipud(input)
         
         data = {'label': label, 'input': input}
+
+        return data
+
+class RandomCrop(object):
+    def __init__(self, shape):
+        self.shape = shape
+    
+    def __call__(self, data):
+        input, label = data['input'], data['label']
+
+        h, w = input.shape[:2]
+        new_h, new_w = self.shape
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        id_y = np.arange(top, top+new_h, 1)[:, np.newaxis]
+        id_x = np.arange(left, left+new_w, 1)
+
+        input = input[id_y, id_x]
+        label = label[id_y, id_x]
+
+        data = { 'input': input, 'label': label }
 
         return data
